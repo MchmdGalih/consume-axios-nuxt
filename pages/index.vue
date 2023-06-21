d6052963f0f50debeb9566d5ae4959b6c6ac20b4
mochamadgalih <mochamadglh@gmail.com>
2023-06-21 12:43:18 +0700

    fix update

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header />
+    <main class="container">
+      <Hero />
+      <div class="row mb-2">
+        <Article
+          v-for="(article, index) in articles"
+          :key="index"
+          :article="article"
+        />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Hero from '../components/Hero.vue'
+import Article from '../components/CardArticle.vue'
+export default {
+  data() {
+    return {
+      articles: [],
+    }
+  },
+  components: {
+    Header,
+    Hero,
+    Article,
+  },
+  async fetch() {
+    await this.$axios.get('v1/cnn-news').then((res) => {
+      this.articles = res.data.data
+      console.log('Artikel =>', this.articles)
+    })
+  },
+}
+</script>
+<style></style>
